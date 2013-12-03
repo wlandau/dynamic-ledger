443a452d9abc1b05ed75dfab17f215f7bf8bf95c
wlandau <will.landau@gmail.com>
2013-12-03 09:17:11 -0600

    free

--- ledger.c
+++ ledger.c
@@ -456,7 +456,7 @@ void print_summary(Ledger *ledger){
         if(l1)
           printf("%0.2f\tpending balance\n", ledger->bank_totals[i][1] 
                                            + ledger->bank_totals[i][2]);   
-        if(l0)
+        if(l2)
           printf("%0.2f\tcleared balance\n", ledger->bank_totals[i][2]);  
       }        
 
