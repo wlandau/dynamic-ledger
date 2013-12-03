da6342fe64c650698e82c3df33dc2f52b824c4f1
wlandau <will.landau@gmail.com>
2013-12-03 09:45:38 -0600

    good

--- ledger.c
+++ ledger.c
@@ -463,7 +463,7 @@ void print_summary(Ledger *ledger){
     if(ledger->npartition[i] && (abs(ledger->leftover[i]) > eps))
       printf("%0.2f\tunpartitioned\n", ledger->leftover[i]); 
  
-    if(l0 || l1 || l2)
+    if(!(ledger->npartition[i]) && (l0 || l1 || l2))
       printf("\n");   
   }
 }
