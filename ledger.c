487c549b65a9bbf478327f371290bfa655b2831d
wlandau <will.landau@gmail.com>
2013-12-03 09:14:08 -0600

    free

--- ledger.c
+++ ledger.c
@@ -416,8 +416,10 @@ void print_summary(Ledger *ledger){
           printf("%0.2f\tnot arrived\n", ledger->credit_totals[i][0]); 
         if(l1)
           printf("%0.2f\tpending\n", ledger->credit_totals[i][1]); 
-        printf("\n");
+        
       } 
+      
+      printf("\n");
       
       printf("%0.2f\ttrue balance\n", ledger->credit_totals[i][3]);
       if(abs(ledger->credit_totals[i][2] - ledger->credit_totals[i][3]) < eps){
@@ -425,7 +427,7 @@ void print_summary(Ledger *ledger){
         if(l1)
           printf("%0.2f\tpending balance\n", ledger->credit_totals[i][1] 
                                            + ledger->credit_totals[i][2]);   
-        if(l0)
+        if(l2)
           printf("%0.2f\tcleared balance\n", ledger->credit_totals[i][2]);  
       }        
 
@@ -456,7 +458,7 @@ void print_summary(Ledger *ledger){
         if(l1)
           printf("%0.2f\tpending balance\n", ledger->bank_totals[i][1] 
                                            + ledger->bank_totals[i][2]);   
-        if(l0)
+        if(l2)
           printf("%0.2f\tcleared balance\n", ledger->bank_totals[i][2]);  
       }        
 
