d3ab641e81d9df48b72b0e54cf2a050fe6c1005e
wlandau <will.landau@gmail.com>
2013-12-03 22:23:37 -0600

    d

--- ledger.c
+++ ledger.c
@@ -821,14 +821,14 @@ int standalone(int argc, char **argv){
   return 0;
 }
 
-int main(int argc, char **argv){ /*
+int main(int argc, char **argv){ 
   return standalone(argc, argv) ? EXIT_FAILURE : EXIT_SUCCESS;
   
-  */
+  /*
   Ledger *ledger = get_ledger_from_filename(argv[1]);
   print_ledger_verbose(ledger);
   
   modify(ledger
-  
+  */
   
 }
