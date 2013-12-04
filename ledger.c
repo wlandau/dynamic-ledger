a9df946ac497c48fc0376328f9de31b9641d08ae
wlandau <will.landau@gmail.com>
2013-12-04 08:31:06 -0600

    last way of printing ledger was more memory efficient. reverting. checked = safe. need to read ledger from string, but also keep using current implementation of reading from stream.

--- ledger.c
+++ ledger.c
@@ -946,14 +946,7 @@ int standalone(int argc, char **argv){
 
   return 0;
 }
-
-int main(int argc, char **argv){ /*
+ 
+int main(int argc, char **argv){
   return standalone(argc, argv) ? EXIT_FAILURE : EXIT_SUCCESS;
-  
-  */
-  
-  Ledger *ledger = get_ledger_from_filename(argv[1]);
-  print_ledger_to_stream(ledger, stdout);
-  free_ledger(ledger); 
-  return 0; 
 }
