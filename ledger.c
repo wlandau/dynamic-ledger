4f4660b1e32f9ce445bdc632da7ec73cf52152c6
wlandau <will.landau@gmail.com>
2013-12-04 11:50:23 -0600

    d

--- ledger.c
+++ ledger.c
@@ -1012,15 +1012,12 @@ int main(int argc, char **argv){ /*
   for(i = 0; i < 20; ++i)
     remove_row(ledger, 0);
   
-  print_ledger_verbose(ledger, stdout);
-  printf("\n\n===============================\n\n");
   
-  /*
     modify(ledger, 0, 3, "15");
  
    print_ledger_verbose(ledger, stdout);
   printf("\n\n===============================\n\n");
-    
+ /*   
         modify(ledger, 0, 0, "15");
  
     print_ledger_verbose(ledger, stdout);
