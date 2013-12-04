b66d43877947236ccc118e7395d039162435afb6
wlandau <will.landau@gmail.com>
2013-12-04 16:45:32 -0600

    time to test

--- ledger.c
+++ ledger.c
@@ -590,7 +590,7 @@ void get_totals(Ledger *ledger){
 }
 
 int safe_get_text_content_from_string(Ledger *ledger, char *s){
-  char *s2 = malloc(sizeof(s));
+  char *s2 = malloc(sizeof(*s));
   int ret;
   strcpy(s2, s);
   ret = get_text_content_from_string(ledger, s2);
