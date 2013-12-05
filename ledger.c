c020b3a82b5321f2703e9b60fd1d913599b01c25
wlandau <will.landau@gmail.com>
2013-12-05 16:41:02 -0600

    what segfault

--- ledger.c
+++ ledger.c
@@ -390,7 +390,7 @@ int get_text_content_from_string(Ledger *ledger, char **s){
     } else if(c == '\n' || c == '\r'){
       field = 0;
       ++row; 
-    } else if(field < NFIELDS && c != 0){
+    } else if(field < NFIELDS && c != '\0'){
       strcat(ledger->text_content[row][field], &c);
       printf("%s\n", ledger->text_content[row][field]);
     }
