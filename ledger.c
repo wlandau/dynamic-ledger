0636381e8fabaf06bcdbd47b2eaa7338855ff056
wlandau <will.landau@gmail.com>
2013-12-05 17:09:20 -0600

    READ AND PRINT FROM STRING WITHOUT ERROR

--- ledger.c
+++ ledger.c
@@ -297,64 +297,6 @@ void strstrip(char *s){ /* taken from a stackoverflow forum */
   return; 
 } 
 
-/* COPYRIGHT NOTE:
- *
- * The function local_strsep() in ledger.c was taken from strsep().
- * The copyright info for strsep() is quoted below and in README.txt.
- *
- * Copyright (c) 1990, 1993
- *	The Regents of the University of California.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-
-char *local_strsep(char **stringp, const char *delim){
-  char *s;
-  const char *spanp;
-  int c, sc;
-  char *tok;
-
-  if ((s = *stringp) == NULL)
-    return (NULL);
-
-  for (tok = s;;){
-    c = *s++;
-    spanp = delim;
-    do {
-      if ((sc = *spanp++) == c){
-        if (c == 0)
-          s = NULL;
-        else
-          s[-1] = 0;
-        *stringp = s;
-        return (tok);
-      }
-    } while (sc != 0);
-  }
-}
-
 int contains_tabs(char *s){
   int i, n = strlen(s);
   for(i = 0; i < n; ++i)
@@ -438,7 +380,7 @@ int get_text_content_from_stream(Ledger *ledger, FILE *fp){
       ++row; 
     } else if(field < NFIELDS && c != '\0'){
       ledger->text_content[field][row][i] = c;
-      ++i;
+      ++i; 
     }
   }
   
@@ -589,7 +531,7 @@ Ledger *get_ledger_from_filename(const char* filename){
     return NULL;
   
   fp = fopen(filename, "r");
-  ledger = get_ledger_from_stream(fp);
+  ledger = get_ledger_from_stream(fp); 
   fclose(fp);
   
   if(ledger == NULL)
