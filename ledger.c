be4e6b373b3d37cc94b03191bbbbb199847dff73
wlandau <will.landau@gmail.com>
2013-12-04 18:49:08 -0600

    adding modify and insert all over again

--- ledger.c
+++ ledger.c
@@ -1051,12 +1051,14 @@ int standalone(int argc, char **argv){
 int main(int argc, char **argv){ /*
   return standalone(argc, argv) ? EXIT_FAILURE : EXIT_SUCCESS; */
   
+
+  
   Ledger *ledger = get_ledger_from_filename(argv[1]);
   
   print_ledger_verbose(ledger, stdout);
   printf("\n\n======================\n\n");
   
-  modify(ledger, 1, 1000, "1sdf5");
+  insert_row(ledger, 5);
   
   print_ledger_verbose(ledger, stdout);
   
