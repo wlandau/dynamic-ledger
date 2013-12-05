d4f93eda03d7d85aab11e608b1ce02011123fc40
wlandau <will.landau@gmail.com>
2013-12-05 15:29:45 -0600

    d

--- ledger.c
+++ ledger.c
@@ -370,7 +370,8 @@ int get_text_content_from_string(Ledger *ledger, char **s){
   if(ledger == NULL || *s == NULL)
     return 1;
     
-  begin = strdup(*s);
+  begin = calloc(strlen(*s) + 1, sizeof(char));
+  strcpy(begin, *s);
 
   ledger->n = 1;
   for(i = 0; i < strlen(*s); ++i)
