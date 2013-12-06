b74467029ffa2c10081077a98ac43402a3502b0b
wlandau <will.landau@gmail.com>
2013-12-05 20:20:25 -0600

    d

--- ledger.c
+++ ledger.c
@@ -1236,7 +1236,7 @@ int standalone(int argc, char **argv){
 int main(int argc, char **argv){ /*
   return standalone(argc, argv) ? EXIT_FAILURE : EXIT_SUCCESS; */
   
-  Ledger *ledger = get_ledger_from_filename(argv[1]);/*
+  Ledger *ledger = get_ledger_from_filename(argv[1]);
   char *s = print_summary_to_string(ledger);
   
   FILE *fp = fopen("ddd.txt", "w");
@@ -1249,8 +1249,9 @@ int main(int argc, char **argv){ /*
     print_summary(ledger, fp);
    fclose(fp);
   
-  free(s);
-    */
+  if(s != NULL)
+    free(s);
+    
    free_ledger(ledger);
         return 0;
 }
