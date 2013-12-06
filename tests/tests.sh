e3c76c29d9f385fa627f09810e52202ae4378872
wlandau <will.landau@gmail.com>
2013-12-05 23:53:36 -0600

    fixed tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,30 +1,30 @@
 git pull
 make -C ..
 
-valgrind ../ledger bad.txt
-valgrind ../ledger bad.txt con.txt
-valgrind ../ledger con.txt 
+valgrind ../ledger-txt bad.txt
+valgrind ../ledger-txt bad.txt con.txt
+valgrind ../ledger-txt con.txt 
 
-valgrind ../ledger empty.txt
-valgrind ../ledger empty.txt con.txt
-valgrind ../ledger con.txt 
+valgrind ../ledger-txt empty.txt
+valgrind ../ledger-txt empty.txt con.txt
+valgrind ../ledger-txt con.txt 
 
-valgrind ../ledger example_ledger.txt
-valgrind ../ledger example_ledger.txt con.txt
-valgrind ../ledger con.txt 
+valgrind ../ledger-txt example_ledger.txt
+valgrind ../ledger-txt example_ledger.txt con.txt
+valgrind ../ledger-txt con.txt 
 
-valgrind ../ledger nonnull.txt
-valgrind ../ledger nonnull.txt con.txt
-valgrind ../ledger con.txt 
+valgrind ../ledger-txt nonnull.txt
+valgrind ../ledger-txt nonnull.txt con.txt
+valgrind ../ledger-txt con.txt 
 
-valgrind ../ledger justheader.txt
-valgrind ../ledger justheader.txt con.txt
-valgrind ../ledger con.txt 
+valgrind ../ledger-txt justheader.txt
+valgrind ../ledger-txt justheader.txt con.txt
+valgrind ../ledger-txt con.txt 
 
-valgrind ../ledger tests.sh
-valgrind ../ledger tests.sh con.txt
-valgrind ../ledger con.txt 
+valgrind ../ledger-txt tests.sh
+valgrind ../ledger-txt tests.sh con.txt
+valgrind ../ledger-txt con.txt 
 
-valgrind ../ledger ssdflks
-valgrind ../ledger adld con.txt
-valgrind ../ledger con.txt 
+valgrind ../ledger-txt ssdflks
+valgrind ../ledger-txt adld con.txt
+valgrind ../ledger-txt con.txt 
