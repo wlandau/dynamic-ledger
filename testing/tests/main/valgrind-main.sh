661c7753460b9f32dcad3605079de7466898c267
wlandau <will.landau@gmail.com>
2013-12-07 00:20:11 -0600

    d

--- testing/tests/main/valgrind-main.sh
+++ testing/tests/main/valgrind-main.sh
@@ -13,6 +13,12 @@ do
   valgrind $DIR/../../../bin/ledger-txt $DIR/../../ledgers/ledgers/$f 1> $DIR/summaries/$f 2>> $DIR/stderr.txt
   valgrind $DIR/../../../bin/ledger-txt $DIR/../../ledgers/ledgers/$f $DIR/condensed/$f 2>> $DIR/stderr.txt
 
+
+  echo $DIR/../../ledgers/condensed/$f 
+echo $DIR/condensed/$f
+
+
+
   diff $DIR/../../ledgers/condensed/$f $DIR/condensed/$f 2>> $DIR/stderr.txt
   diff $DIR/../../ledgers/summaries/$f $DIR/summaries/$f 2>> $DIR/stderr.txt
 done
