60a0285ba3c9ab097fff23bd0912d072f005d851
wlandau <will.landau@gmail.com>
2013-12-09 20:05:02 -0600

    d

---
+++ src/which.c
@@ -0,0 +1,17 @@
+/***
+ *** @file which.c
+ *** @author Will Landau
+ *** @email will.landau@gmail.com
+ ***/
+
+#include <errno.h>
+#include <getopt.h>
+#include <ledger.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <user_settings.h>
+
+int which(const char **s){
+  return 0;
+}
