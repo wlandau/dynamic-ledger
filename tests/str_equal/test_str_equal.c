e596f3d100415585fd7409271e8e2d2d39221da2
wlandau <will.landau@gmail.com>
2013-12-09 20:16:00 -0600

    d

--- tests/str_equal/test_str_equal.c
+++ tests/str_equal/test_str_equal.c
@@ -23,48 +23,48 @@ int main(){
   *e = malloc(100 * sizeof(char));   
   *f = malloc(100 * sizeof(char));
   
-  strcpy(*a, "good");
-  strcpy(*b, "badd");
-  strcpy(*c, "gooda");
+  strcpy(*a, "gd");
+  strcpy(*b, "bad");
+  strcpy(*c, "gda");
   strcpy(*d, "\0");
   strcpy(*e, "\0lk;sd");
   strcpy(*f, "");
   *g = NULL;
    
-  printf("%s\t%s\t%d\n", *a, *a, str_equal(* a, *a));
-  printf("%s\t%s\t%d\n", *a, *b, str_equal(* a, *b));
-  printf("%s\t%s\t%d\n", *a, *c, str_equal(* a, *c));
-  printf("%s\t%s\t%d\n", *a, *d, str_equal(* a, *d));
-  printf("%s\t%s\t%d\n\n", *a, *e, str_equal(* a, *e));
-  printf("%s\t%s\t%d\n\n", *a, *f, str_equal(* a, *f));
-  printf("%s\t%s\t%d\n\n", *a, *g, str_equal(* a, *g));
+  printf("a=%s\ta=%s\t%d\n", *a, *a, str_equal(* a, *a));
+  printf("a=%s\tb=%s\t%d\n", *a, *b, str_equal(* a, *b));
+  printf("a=%s\tc=%s\t%d\n", *a, *c, str_equal(* a, *c));
+  printf("a=%s\td=%s\t%d\n", *a, *d, str_equal(* a, *d));
+  printf("a=%s\te=%s\t%d\n", *a, *e, str_equal(* a, *e));
+  printf("a=%s\tf=%s\t%d\n", *a, *f, str_equal(* a, *f));
+  printf("a=%s\tg=%s\t%d\n\n", *a, *g, str_equal(* a, *g));
   
-  printf("%s\t%s\t%d\n", *b, *b, str_equal(* b, *b));
-  printf("%s\t%s\t%d\n", *b, *c, str_equal(* b, *c));
-  printf("%s\t%s\t%d\n", *b, *d, str_equal(* b, *d));
-  printf("%s\t%s\t%d\n\n", *b, *e, str_equal(* b, *e));
-  printf("%s\t%s\t%d\n\n", *b, *f, str_equal(* b, *f));
-  printf("%s\t%s\t%d\n\n", *b, *g, str_equal(* b, *g));
+  printf("b=%s\tb=%s\t%d\n", *b, *b, str_equal(* b, *b));
+  printf("b=%s\tc=%s\t%d\n", *b, *c, str_equal(* b, *c));
+  printf("b=%s\td=%s\t%d\n", *b, *d, str_equal(* b, *d));
+  printf("b=%s\te=%s\t%d\n", *b, *e, str_equal(* b, *e));
+  printf("b=%s\tf=%s\t%d\n", *b, *f, str_equal(* b, *f));
+  printf("b=%s\tg=%s\t%d\n\n", *b, *g, str_equal(* b, *g));
 
-  printf("%s\t%s\t%d\n", *c, *c, str_equal(* c, *c));
-  printf("%s\t%s\t%d\n", *c, *d, str_equal(* c, *d));
-  printf("%s\t%s\t%d\n\n", *c, *e, str_equal(* c, *e));
-  printf("%s\t%s\t%d\n\n", *c, *f, str_equal(* c, *f));
-  printf("%s\t%s\t%d\n\n", *c, *g, str_equal(* c, *g));
+  printf("c=%s\tc=%s\t%d\n", *c, *c, str_equal(* c, *c));
+  printf("c=%s\td=%s\t%d\n", *c, *d, str_equal(* c, *d));
+  printf("c=%s\te=%s\t%d\n", *c, *e, str_equal(* c, *e));
+  printf("c=%s\tf=%s\t%d\n", *c, *f, str_equal(* c, *f));
+  printf("c=%s\tg=%s\t%d\n\n", *c, *g, str_equal(* c, *g));
 
-  printf("%s\t%s\t%d\n", *d, *d, str_equal(* d, *d));
-  printf("%s\t%s\t%d\n\n", *d, *e, str_equal(* d, *e));
-  printf("%s\t%s\t%d\n\n", *d, *f, str_equal(* d, *f));
-  printf("%s\t%s\t%d\n\n", *d, *g, str_equal(* d, *g));
+  printf("d=%s\td=%s\t%d\n", *d, *d, str_equal(* d, *d));
+  printf("d=%s\te=%s\t%d\n", *d, *e, str_equal(* d, *e));
+  printf("d=%s\tf=%s\t%d\n", *d, *f, str_equal(* d, *f));
+  printf("d=%s\tg=%s\t%d\n\n", *d, *g, str_equal(* d, *g));
 
-  printf("%s\t%s\t%d\n\n", *e, *e, str_equal(* e, *e));
-  printf("%s\t%s\t%d\n\n", *e, *f, str_equal(* e, *f));
-  printf("%s\t%s\t%d\n\n", *e, *g, str_equal(* e, *g));
+  printf("e=%s\te=%s\t%d\n", *e, *e, str_equal(* e, *e));
+  printf("e=%s\tf=%s\t%d\n", *e, *f, str_equal(* e, *f));
+  printf("e=%s\tg=%s\t%d\n\n", *e, *g, str_equal(* e, *g));
   
-  printf("%s\t%s\t%d\n\n", *f, *f, str_equal(* f, *f));
-  printf("%s\t%s\t%d\n\n", *f, *g, str_equal(* f, *g));
+  printf("f=%s\tf=%s\t%d\n", *f, *f, str_equal(* f, *f));
+  printf("f=%s\tg=%s\t%d\n\n", *f, *g, str_equal(* f, *g));
 
-  printf("%s\t%s\t%d\n\n", *g, *g, str_equal(* g, *g));
+  printf("g=%s\tg=%s\t%d\n\n", *g, *g, str_equal(* g, *g));
   
   free(*a);
   free(*b);
