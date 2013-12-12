208ac8dfb3751d6daf3d18c1b3240b67fc425824
wlandau <will.landau@gmail.com>
2013-12-12 09:29:05 -0600

    d

---
+++ tests/alloc_entries/test_alloc_entries.c
@@ -0,0 +1,33 @@
+#include <errno.h>
+#include <getopt.h>
+#include <ledger.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <user_settings.h>
+
+int main(){
+  int i, j, ret;
+  Ledger *ledger = NULL;
+  
+  ret = alloc_entries(ledger);
+  printf("\n----\nRETURN VALUE = %d\n---\n", ret);
+
+  ledger = malloc(sizeof(Ledger));
+  ledger->nrows = 0;
+  ret = alloc_entries(ledger);
+  printf("\n----\nRETURN VALUE = %d\n---\n", ret);
+  
+  ledger->nrows = 4;
+  ret = alloc_entries(ledger);
+  printf("\n----\nRETURN VALUE = %d\n---\n", ret);
+  
+  for(i = 0; i < NFIELDS; ++i){
+    for(j = 0; j < ledger->nrows; ++j)
+      printf("e[%d][%d]=%s ", i, j, ledger->entries[i][j]);
+    printf("\n");
+  }
+  
+  free_ledger(ledger);
+  return 0;
+}
