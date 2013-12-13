6bea35697f27fa4885ae63b8f583879259833441
wlandau <will.landau@gmail.com>
2013-12-12 20:47:20 -0600

    d

---
+++ src/strip_ledger.c
@@ -0,0 +1,29 @@
+/***
+ *** @file strip_ledger.c
+ *** @author Will Landau
+ *** @email will.landau@gmail.com
+ ***/
+
+#include <errno.h>
+#include <getopt.h>
+#include <ledger.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <user_settings.h>
+
+err_t strip_ledger(Ledger *ledger){
+  int i, j;
+  
+  if(ledger == NULL)
+    return LFAILURE;
+
+  if(ledger->nrows < 1)
+    return LFAILURE;
+
+  for(i = 0; i < NFIELDS; ++i)
+    for(j = 0; j < ledger->nrows; ++j)
+      str_strip(ledger->entries[i][j]);
+  
+  return LSUCCESS;
+}
