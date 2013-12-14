817112de831450198eaec176e55f8a898f2b2bc9
wlandau <will.landau@gmail.com>
2013-12-14 10:31:30 -0600

    d

--- tests/print_summary_to_string/test_print_summary_to_string.c
+++ tests/print_summary_to_string/test_print_summary_to_string.c
@@ -13,7 +13,7 @@ int main(){
   Ledger *ledger = NULL;
   char *s = NULL;
   
-  printf("\n----\nRET = %d\n---\n", print_summary_to_string(ledger, &s));
+  printf("\n----\nRET = %d\n---\n", print_summary_to_string(ledger, &s, 1));
   printf("%s", s);  
   if(s != NULL){
     free(s);
@@ -21,7 +21,7 @@ int main(){
   }
     
   new_ledger(&ledger);
-  printf("\n----\nRET = %d\n---\n", print_summary_to_string(ledger, &s));
+  printf("\n----\nRET = %d\n---\n", print_summary_to_string(ledger, &s, 1));
   free_ledger(&ledger);
   printf("%s", s);  
   if(s != NULL){
@@ -32,7 +32,7 @@ int main(){
   new_ledger(&ledger);
   get_entries_from_filename(ledger, L2); 
   get_names(ledger);
-  printf("\n----\nRET = %d\n---\n", print_summary_to_string(ledger, &s));
+  printf("\n----\nRET = %d\n---\n", print_summary_to_string(ledger, &s, 1));
   free_ledger(&ledger); 
   printf("%s", s);  
   if(s != NULL){
@@ -44,7 +44,7 @@ int main(){
   get_entries_from_filename(ledger, L2); 
   get_names(ledger);
   get_totals(ledger);
-  printf("\n----\nRET = %d L2\n---\n", print_summary_to_string(ledger, &s));
+  printf("\n----\nRET = %d L2\n---\n", print_summary_to_string(ledger, &s, 1));
   free_ledger(&ledger);
   printf("%s", s);  
   if(s != NULL){
@@ -55,7 +55,7 @@ int main(){
   new_ledger(&ledger);    
   get_entries_from_filename(ledger, L3); 
   get_names(ledger);
-  printf("\n----\nRET = %d\n---\n", print_summary_to_string(ledger, &s));
+  printf("\n----\nRET = %d\n---\n", print_summary_to_string(ledger, &s, 1));
   free_ledger(&ledger); 
   printf("%s", s);  
   if(s != NULL){
@@ -67,7 +67,19 @@ int main(){
   get_entries_from_filename(ledger, L3); 
   get_names(ledger);
   get_totals(ledger);
-  printf("\n----\nRET = %d L3\n---\n", print_summary_to_string(ledger, &s));
+  printf("\n----\nRET = %d L3\n---\n", print_summary_to_string(ledger, &s, 1));
+  free_ledger(&ledger);
+  printf("%s", s);  
+  if(s != NULL){
+    free(s);
+    s = NULL;
+  } 
+  
+  new_ledger(&ledger);
+  get_entries_from_filename(ledger, L3); 
+  get_names(ledger);
+  get_totals(ledger);
+  printf("\n----\nRET = %d L3\n---\n", print_summary_to_string(ledger, &s, 0));
   free_ledger(&ledger);
   printf("%s", s);  
   if(s != NULL){
