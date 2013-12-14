f73b4c8e2fee93502aba82395ed85a45f959248a
wlandau <will.landau@gmail.com>
2013-12-14 10:30:28 -0600

    d

--- tests/print_summary_to_stream/test_print_summary_to_stream.c
+++ tests/print_summary_to_stream/test_print_summary_to_stream.c
@@ -12,38 +12,45 @@
 int main(){
   Ledger *ledger = NULL;
   
-  printf("\n----\nRET = %d\n---\n", print_summary_to_stream(ledger, stdout));
+  printf("\n----\nRET = %d\n---\n", print_summary_to_stream(ledger, stdout, 1));
     
   new_ledger(&ledger);
-  printf("\n----\nRET = %d\n---\n", print_summary_to_stream(ledger, stdout));
+  printf("\n----\nRET = %d\n---\n", print_summary_to_stream(ledger, stdout, 1));
   free_ledger(&ledger);
 
   new_ledger(&ledger);
   get_entries_from_filename(ledger, L2); 
   get_names(ledger);
-  printf("\n----\nRET = %d\n---\n", print_summary_to_stream(ledger, stdout));
+  printf("\n----\nRET = %d\n---\n", print_summary_to_stream(ledger, stdout, 1));
   free_ledger(&ledger); 
 
   new_ledger(&ledger);
   get_entries_from_filename(ledger, L2); 
   get_names(ledger);
   get_totals(ledger);
-  printf("\n----\nRET = %d L2\n---\n", print_summary_to_stream(ledger, stdout));
+  printf("\n----\nRET = %d L2\n---\n", print_summary_to_stream(ledger, stdout, 1));
   free_ledger(&ledger);
 
   new_ledger(&ledger);    
   get_entries_from_filename(ledger, L3); 
   get_names(ledger);
-  printf("\n----\nRET = %d\n---\n", print_summary_to_stream(ledger, stdout));
+  printf("\n----\nRET = %d\n---\n", print_summary_to_stream(ledger, stdout, 1));
   free_ledger(&ledger); 
 
   new_ledger(&ledger);
   get_entries_from_filename(ledger, L3); 
   get_names(ledger);
   get_totals(ledger);
-  printf("\n----\nRET = %d L3\n---\n", print_summary_to_stream(ledger, stdout));
+  printf("\n----\nRET = %d L3\n---\n", print_summary_to_stream(ledger, stdout, 1));
   free_ledger(&ledger);
   
+  
+  new_ledger(&ledger);
+  get_entries_from_filename(ledger, L3); 
+  get_names(ledger);
+  get_totals(ledger);
+  printf("\n----\nRET = %d L3\n---\n", print_summary_to_stream(ledger, stdout, 0));
+  free_ledger(&ledger);
   
 
 
