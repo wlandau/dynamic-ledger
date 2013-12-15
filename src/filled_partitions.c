979c4e1fec6b46445ccb8e441f4842c02d29d006
wlandau <will.landau@gmail.com>
2013-12-15 10:35:50 -0600

    d

---
+++ src/filled_partitions.c
@@ -0,0 +1,20 @@
+/***
+ *** @file filled_partitions.c
+ *** @author Will Landau
+ *** @email will.landau@gmail.com
+ ***/
+
+#include <errno.h>
+#include <getopt.h>
+#include <ledger.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <user_settings.h>
+
+bool_t filled_partitions(Ledger *ledger){
+
+
+
+}
---
+++ src/untotaled.c
@@ -0,0 +1,30 @@
+/***
+ *** @file totaled.c
+ *** @author Will Landau
+ *** @email will.landau@gmail.com
+ ***/
+
+#include <errno.h>
+#include <getopt.h>
+#include <ledger.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <user_settings.h>
+
+
+bool_t untotaled(Ledger *ledger){
+  if(ledger == NULL)
+    return LYES;
+    
+  if(ledger->banks == NULL || ledger->credits == NULL || ledger->partitions == NULL ||
+     !ledger->nbanks || !ledger->ncredits || ledger->npartitions == NULL)  
+    return LYES;
+  
+  if(ledger->bank_totals == NULL || ledger->credit_totals == NULL || 
+     ledger->partition_totals == NULL)
+    return LYES;
+    
+  return LNO;
+}
