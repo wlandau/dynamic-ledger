3328ddc49989fd9dcef5cf4fab4bea72809e317e
wlandau <will.landau@gmail.com>
2013-12-14 22:17:48 -0600

    d

---
+++ tests/rename_credit/test_rename_credit.c
@@ -0,0 +1,27 @@
+#include <errno.h>
+#include <getopt.h>
+#include <ledger.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <user_settings.h>
+
+#define F "../data/ledgers/basic.txt"
+
+int main(){
+  Ledger *ledger = NULL;
+ 
+  printf("TEST 1 RET = %d\n", rename_credit(ledger, "hi", "ho"));
+  
+  get_ledger(&ledger, F, NULL, NULL);
+  
+  print_ledger_verbose(ledger, stdout);
+  printf("\nTEST 2 RET = %d\n", rename_credit(ledger, "hi", "ho"));
+
+  print_ledger_verbose(ledger, stdout);
+  printf("\nTEST 3 RET = %d\n", rename_credit(ledger, "2nd", "hi"));
+  print_ledger_verbose(ledger, stdout);
+
+  free_ledger(&ledger);
+  return 0;
+}
