163920c686cd9ba8792660fe7f69832a3282cb9f
wlandau <will.landau@gmail.com>
2013-12-17 16:09:56 -0600

    d

--- src/move_rows.c
+++ src/move_rows.c
@@ -15,6 +15,8 @@
 err_t move_rows(Ledger *ledger, int *rows, int nrows, int moveto){
   int i, *order;
   
+  /* CHECK FOR NULL INPUT */
+  
   if(ledger == NULL || rows == NULL)
     return LFAILURE;
     
@@ -24,18 +26,30 @@ err_t move_rows(Ledger *ledger, int *rows, int nrows, int moveto){
   if(ledger->nrows < 2)
     return LSUCCESS;
   
+  /* ALLOCATE SPACE FOR PERMUTATION ORDER AND CHECK IF MALLOC WORKED */
+  
   order = calloc(ledger->nrows, sizeof(int));
+  if(order == NULL){
+    fprintf(stderr, "Error: malloc failed.\n");
+    return LFAILURE;
+  }
+  
+  /* CALCULATE PERMUTATION */
   
   for(i = moveto; i < ledger->nrows; ++i)
     order[i] = 2;
   
   for(i = 0; i < nrows; ++i)
     order[rows[i]] = 1;
+
+  /* APPLY PERMUTATION */
   
   if(permute_rows(ledger, order) == LFAILURE){
     free(order);
     return LFAILURE;
   }
+  
+  /* CLEAN UP AND RETURN */
   
   free(order);
   return LSUCCESS;
