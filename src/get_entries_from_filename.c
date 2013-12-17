8ff52a3c95172dee7d8c169803f418d3ef216a42
wlandau <will.landau@gmail.com>
2013-12-17 15:45:48 -0600

    d

--- src/get_entries_from_filename.c
+++ src/get_entries_from_filename.c
@@ -14,10 +14,14 @@
 
 err_t get_entries_from_filename(Ledger *ledger, char *filename){
   err_t ret;
-  FILE *fp;
+  FILE *fp = NULL;
+  
+  /* CHECK IF INPUT FILE NAME IS GOOD */
   
   if(input_file(filename) == LNO)
     return LFAILURE;
+
+  /* OPEN FILE AND STORE FILE NAME */
     
   fp = fopen(filename, "r");
   if(fp == NULL)
@@ -27,7 +31,20 @@ err_t get_entries_from_filename(Ledger *ledger, char *filename){
     ledger->filename = malloc(FILENAMESIZE * sizeof(char));
   strcpy(ledger->filename, filename);
   
+  /* CHECK IF MALLOC WORKED */
+  
+  if(ledger->filename == NULL){
+    fprintf(stderr, "Error: malloc failed\n");
+    if(fp != NULL)
+      fclose(fp);
+    return LFAILURE;
+  }
+  
+  /* GET THE LEDGER ENTRIES FROM THE FILE STREAM */
+  
   ret = get_entries_from_stream(ledger, fp);
+
+  /* CLEAN UP AND EXIT */
     
   if(fp != NULL)
     fclose(fp);
