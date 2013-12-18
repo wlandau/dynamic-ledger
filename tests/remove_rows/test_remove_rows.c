0f9682398ecd604c10b09c5a69239138aecd63f9
wlandau <will.landau@gmail.com>
2013-12-17 20:21:16 -0600

    d

--- tests/remove_rows/test_remove_rows.c
+++ tests/remove_rows/test_remove_rows.c
@@ -16,24 +16,26 @@ int main(){
   printf("\n--------\nTEST 1 | ret = %d\n",  remove_rows(ledger));
 
   get_ledger(&ledger, F, NULL, NULL);
-  print_ledger_verbose(ledger, stdout);
+  print_ledger_to_stream(ledger, stdout);
+  print_summary_to_stream(ledger, stdout, 1);
+  
   printf("\n--------\nTEST 2 | ret = %d\n",  remove_rows(ledger));
-  print_ledger_verbose(ledger, stdout);
+  print_ledger_to_stream(ledger, stdout);
+  print_summary_to_stream(ledger, stdout, 1);
   
   for(i = 0; i < ledger->nrows; ++i)
     if(i == 2 || i == 6 || i == 7 || i == 8)
       strcpy(ledger->entries[STATUS][i], REMOVE);
    
   printf("\n--------\nTEST 3 | ret = %d\n",  remove_rows(ledger));
-  print_ledger_verbose(ledger, stdout);
-  free_ledger(&ledger);
-  
-  
-  get_ledger(&ledger, F, NULL, NULL);
+  print_ledger_to_stream(ledger, stdout);
+  print_summary_to_stream(ledger, stdout, 1);
+
   for(i = 0; i < ledger->nrows; ++i)
     strcpy(ledger->entries[STATUS][i], REMOVE);
-  printf("\n--------\nTEST 3 | ret = %d\n",  remove_rows(ledger));
-  print_ledger_verbose(ledger, stdout);
+  printf("\n--------\nTEST 4 | ret = %d\n",  remove_rows(ledger));
+  print_ledger_to_stream(ledger, stdout);
+  print_summary_to_stream(ledger, stdout, 1);
   
   free_ledger(&ledger);  
   return 0;
