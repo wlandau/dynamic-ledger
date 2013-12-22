d7b0aa128f3c74f9f601e37c6d404a66ebb1cdcb
wlandau <will.landau@gmail.com>
2013-12-22 00:00:39 -0600

    d

--- tests/map_to_column/test_map_to_column.c
+++ tests/map_to_column/test_map_to_column.c
@@ -10,7 +10,7 @@
 
 int main(){
   Ledger *ledger = NULL;
-  int rows[] = {-1, 0, 1, 2, 10};
+  int rows[] = {-1, 0, 2, 10};
   char entrynumber[] = "9";
   char entrystr[] = "  sacrebleu!";
   
@@ -18,37 +18,37 @@ int main(){
   print_ledger_to_stream(ledger, stdout);
   print_summary_to_stream(ledger, stdout, 1);
 
-  printf("1 RET = %d\n", map_to_column(ledger, entrynumber, rows, 5, 0, 1));
+  printf("1 RET = %d\n", map_to_column(ledger, entrynumber, rows, 4, 0, 1));
   print_ledger_to_stream(ledger, stdout);
   print_summary_to_stream(ledger, stdout, 1);
 
-  printf("2 RET = %d\n", map_to_column(ledger, entrynumber, NULL, 5, 0, 1));
+  printf("2 RET = %d\n", map_to_column(ledger, entrynumber, NULL, 4, 0, 1));
   print_ledger_to_stream(ledger, stdout);
   print_summary_to_stream(ledger, stdout, 1);
  
-  printf("3 RET = %d\n", map_to_column(ledger, entrynumber, rows, 5, 0, 2));
+  printf("3 RET = %d\n", map_to_column(ledger, entrynumber, rows, 4, 0, 2));
   print_ledger_to_stream(ledger, stdout);
   print_summary_to_stream(ledger, stdout, 1);
   
-  printf("4 RET = %d\n", map_to_column(ledger, entrynumber, rows, 5, 0, 0));
-  print_ledger_to_stream(ledger, stdout);
-  print_summary_to_stream(ledger, stdout, 1);
-
-  
-  printf("4 RET = %d\n", map_to_column(ledger, entrystr, rows, 5, 5, 0));
-  print_ledger_to_stream(ledger, stdout);
-  print_summary_to_stream(ledger, stdout, 1);
-  
-  printf("5 RET = %d\n", map_to_column(ledger, entrystr, rows, 5, 5, 2));
+  printf("4 RET = %d\n", map_to_column(ledger, entrynumber, rows, 4, 0, 0));
   print_ledger_to_stream(ledger, stdout);
   print_summary_to_stream(ledger, stdout, 1);
 
   
-  printf("6 RET = %d\n", map_to_column(ledger, entrystr, rows, 5, 1, 2));
+  printf("4 RET = %d\n", map_to_column(ledger, entrystr, rows, 4, 5, 0));
   print_ledger_to_stream(ledger, stdout);
   print_summary_to_stream(ledger, stdout, 1);
   
-  printf("7 RET = %d\n", map_to_column(ledger, entrystr, rows, 5, 2, 1));
+  printf("5 RET = %d\n", map_to_column(ledger, entrystr, rows, 4, 5, 2));
+  print_ledger_to_stream(ledger, stdout);
+  print_summary_to_stream(ledger, stdout, 1);
+
+  
+  printf("6 RET = %d\n", map_to_column(ledger, entrystr, rows, 4, 1, 2));
+  print_ledger_to_stream(ledger, stdout);
+  print_summary_to_stream(ledger, stdout, 1);
+  
+  printf("7 RET = %d\n", map_to_column(ledger, entrystr, rows, 4, 2, 1));
   print_ledger_to_stream(ledger, stdout);
   print_summary_to_stream(ledger, stdout, 1);
   
