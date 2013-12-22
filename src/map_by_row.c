f1aa92dad0d27f3748c66d840c059ff5fe3e60ca
wlandau <will.landau@gmail.com>
2013-12-22 15:09:06 -0600

    d

---
+++ src/map_by_row.c
@@ -0,0 +1,35 @@
+/**
+ * @file        map_to_multiple.c
+ * @author      Will Landau (http://www.will-landau.com/)
+ * @date        2013-2014
+ * @copyright   GNU General Public License 3.0
+ */
+
+#include <errno.h>
+#include "ledger.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "user_settings.h"
+
+/**
+ * @details Map a single character string to multiple entries 
+ *          of a Ledger object. Specifically, 
+ *          ledger->entries[coords[i][0]][coords[i][1]] 
+ *          is replaced with "entry", for i from 0 to length(coords) - 1.
+ *          Same as map_by_col, but different data storage format. Set "append"
+ *          to 0 to overwrite each entry, 1 to append to the head of each entry,
+ *          and 2 to append to the tail of each entry. 
+ */
+
+err_t map_by_row(Ledger *ledger, char *entry, int *coords, int howmany, int append){
+  int i;
+  for(i = 0; i < howmany; ++i)
+    if(edit_entry_noretotal(ledger, entry, rows[i], fields[i], append) == LFAILURE) 
+      continue;
+      
+  if(retotal(ledger) == LFAILURE)
+    return LFAILURE; 
+      
+  return LSUCCESS;
+}
---
+++ src/map_to_coords_1.c
@@ -0,0 +1,34 @@
+/**
+ * @file        map_to_multiple.c
+ * @author      Will Landau (http://www.will-landau.com/)
+ * @date        2013-2014
+ * @copyright   GNU General Public License 3.0
+ */
+
+#include <errno.h>
+#include "ledger.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "user_settings.h"
+
+/**
+ * @details Map a single character string to multiple entries 
+ *          of a Ledger object. Specifically, ledger->entries[fields[i]][rows[i]] 
+ *          is replaced with "entry", for i from 0 to length(rows) - 1. Set "append"
+ *          to 0 to overwrite each entry, 1 to append to the head of each entry,
+ *          and 2 to append to the tail of each entry. 
+ */
+
+err_t map_to_coords_1(Ledger *ledger, char *entry, int *rows, int *fields, 
+                    int howmany, int append){
+  int i;
+  for(i = 0; i < howmany; ++i)
+    if(edit_entry_noretotal(ledger, entry, rows[i], fields[i], append) == LFAILURE) 
+      continue;
+      
+  if(retotal(ledger) == LFAILURE)
+    return LFAILURE; 
+      
+  return LSUCCESS;
+}
