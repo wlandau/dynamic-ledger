633648693de21cb8fa17ccb33aea395373b540c2
wlandau <will.landau@gmail.com>
2013-12-22 15:23:21 -0600

    d

---
+++ src/map_to_coords_colmajor.c
@@ -0,0 +1,39 @@
+/**
+ * @file        map_to_coords_colmajor.c
+ * @author      Will Landau (http://www.will-landau.com/)
+ * @date        2013-2014
+ * @copyright   GNU General Public License 3.0
+ */
+
+#include <errno.h>
+#include "ledger.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "user_settings.h"
+
+/**
+ * @details Map a single character string to multiple entries 
+ *          of a Ledger object. Specifically, ledger->entries[coords[0][i]][rows[1][i]] 
+ *          is replaced with "entry", for i from 0 to length(coords) - 1. Set "append"
+ *          to 0 to overwrite each entry, 1 to append to the head of each entry,
+ *          and 2 to append to the tail of each entry. 
+ */
+
+err_t map_to_coords_colmajor(Ledger *ledger, char *entry, int **coords, 
+                    int howmany, int append){
+  int i;
+  
+  if(coords == NULL)
+    return LFAILURE;
+  
+  for(i = 0; i < howmany; ++i)
+    if(coords[i] != NULL)
+      if(edit_entry_noretotal(ledger, entry, coords[0][1], coords[1][i], append) == LFAILURE) 
+        continue;
+      
+  if(retotal(ledger) == LFAILURE)
+    return LFAILURE; 
+      
+  return LSUCCESS;
+}
---
+++ tests/map_to_coords/test_map_to_coords.c
@@ -0,0 +1,59 @@
+#include <errno.h>
+#include <getopt.h>
+#include <ledger.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <user_settings.h>
+
+#define F "../data/ledgers/simple.txt"
+
+int main(){
+  Ledger *ledger = NULL;
+  int rows[] = {-1, 0, 0, 2, 3, 10};
+  int cols[] = {-1, 0, 1, 2, 4, 10};
+  char entrynumber[] = "9";
+  char entrystr[] = "  sacrebleu!";
+  
+  get_ledger(&ledger, F, NULL, NULL);
+  print_ledger_to_stream(ledger, stdout);
+  print_summary_to_stream(ledger, stdout, 1);
+
+  printf("1 RET = %d\n", map_to_coords(ledger, entrynumber, rows, cols, 6, 1));
+  print_ledger_to_stream(ledger, stdout);
+  print_summary_to_stream(ledger, stdout, 1);
+
+  printf("2 RET = %d\n", map_to_coords(ledger, entrynumber, NULL, cols, 0, 1));
+  print_ledger_to_stream(ledger, stdout);
+  print_summary_to_stream(ledger, stdout, 1);
+ 
+  printf("3 RET = %d\n", map_to_coords(ledger, entrynumber, rows, cols, 6, 2));
+  print_ledger_to_stream(ledger, stdout);
+  print_summary_to_stream(ledger, stdout, 1);
+
+  printf("4 RET = %d\n", map_to_coords(ledger, entrynumber, rows, cols, 6, 0));
+  print_ledger_to_stream(ledger, stdout);
+  print_summary_to_stream(ledger, stdout, 1);
+
+  
+  printf("4 RET = %d\n", map_to_coords(ledger, entrystr, rows, cols, 6, 0));
+  print_ledger_to_stream(ledger, stdout);
+  print_summary_to_stream(ledger, stdout, 1);
+  
+  printf("5 RET = %d\n", map_to_coords(ledger, entrystr, rows, cols, 6, 2));
+  print_ledger_to_stream(ledger, stdout);
+  print_summary_to_stream(ledger, stdout, 1);
+
+  
+  printf("6 RET = %d\n", map_to_coords(ledger, entrystr, rows, cols, 6, 2));
+  print_ledger_to_stream(ledger, stdout);
+  print_summary_to_stream(ledger, stdout, 1);
+  
+  printf("7 RET = %d\n", map_to_coords(ledger, entrystr, rows, cols, 6, 1));
+  print_ledger_to_stream(ledger, stdout);
+  print_summary_to_stream(ledger, stdout, 1);
+  
+  free_ledger(&ledger);
+
+  return 0;
+}
